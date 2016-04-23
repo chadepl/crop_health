0b335d6581e063b6aa2503b9b163a6b681216c82
Nicolas Chaves <Nicolas@Nicolass-MacBook-Air.local>
2016-04-23 15:28:00 -0500

    Updated script for reading and processing NIR and RGB images

--- scripts/data_setup/lecturaCorrecta/image.py
+++ scripts/data_setup/lecturaCorrecta/image.py
@@ -1,23 +1,92 @@
 from skimage.external import tifffile as tif
-from skimage import io, filters
+from skimage import (io, filters, transform)
+from skimage import color as color
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.misc as sp
 
-imNIR = io.imread("nir_sa.tif")
-#imRGB = tif.imread("rgb_sa.tif")
+SUMMARY = True
+SAVE_NIR = True
+SAVE_RGB = False
+VIEW_NIR = False
+VIEW_RGB = False
 
-newImg = threshold_adaptive(imNIR,15)
+imNIR = io.imread("nir_sa.tif", plugin = "tifffile")
+imNIR = imNIR[:,:,0:3]
+imRGB = io.imread("rgb_sa.tif", plugin = "tifffile")
+imRGB = imRGB[:,:,0:3]
 
-##print "Data type: ",type(im)
-#print "Shape: "
-#print im.shape()
-#print "Size: "
-#print im.size()
-#for index,(x,y,z), value in np.ndenumerate(im):
-#    print "Index"+str(index)+str(x)+str(y)+str(x)+" Value: "+str(value)
-#tif.imshow(imNIR)
-#tif.imshow(imRGB)
+#Image information
 
-io.imshow(newImg)
+if SUMMARY:
 
-io.show()
+	print "RGB"
+	print "--------------------------"
+	print "Data type: ", imRGB.dtype
+	print "Dimensions: ", imRGB.shape
+	print "Elements in the array: ", imRGB.size
+
+	print "***************************"
+
+	print "NIR"
+	print "--------------------------"
+	print "Data type: ", imNIR.dtype
+	print "Dimensions: ", imNIR.shape
+	print "Elements in the array: ", imNIR.size
+
+	newNIR = transform.resize(imNIR,imRGB.shape)
+
+	print "***************************"
+	print "***************************"
+
+	print "Transformed NIR"
+	print "--------------------------"
+	print "Data type: ", newNIR.dtype
+	newNIR.dtype = "uint8"
+	print "Changed type: ", newNIR.dtype
+	print "Dimensions: ", newNIR.shape
+	print "Elements in the array: ", newNIR.size
+
+#Save NIR in text file
+
+if SAVE_NIR:
+	f = open("nirinfo.txt","w")
+	for x in range(newNIR.shape[0]):
+		for y in range(newNIR.shape[1]):
+			R = newNIR[x,y,0]
+			G = newNIR[x,y,1]
+			B = newNIR[x,y,1]
+			line = "The point ("+str(x)+","+str(y)+") has RGB equal to: ("+str(R)+","+str(G)+","+str(B)+")" 
+			if R != 0 and G != 0 and B != 0:
+				print line
+			f.write(line)
+			f.write("\n")
+
+	f.close()
+	print "Done with NIR"
+
+#Save RGB in text file
+
+if SAVE_RGB:
+	f = open("rgbinfo.txt","w")
+	for x in range(imRGB.shape[0]):
+		for y in range(imRGB.shape[1]):
+			R = imRGB[x,y,0]
+			G = imgRGB[x,y,1]
+			B = imgRGB[x,y,1]
+			line = "The point ("+str(x)+","+str(y)+") has RGB equal to: ("+str(R)+","+str(G)+","+str(B)+")" 
+			if R != 0 and G != 0 and B != 0:
+				print line
+			f.write(line)
+			f.write("\n")
+
+	f.close()
+	print "Done with RGB"
+
+if VIEW_NIR:
+	io.imshow(imNIR)
+	io.show()
+
+if VIEW_RGB:
+	io.imshow(imNIR)
+	io.show()
